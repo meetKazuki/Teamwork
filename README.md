cf054d7701216e822b413a3d732529fffdac9363
Desmond Edem <edemintech@gmail.com>
2019-11-12 20:12:22 +0100

    chore(setup): setup project for development (#1)
    * setup server-side of the application
    * setup testing framework
    * integrate TravisCI
    * integrate Coveralls
    * integrate CodeClimate

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Teamwork
 an internal social network for employees of an organization.
+
+[![Build Status](https://travis-ci.org/meetKazuki/Teamwork.svg?branch=develop)](https://travis-ci.org/meetKazuki/Teamwork)
+[![Coverage Status](https://coveralls.io/repos/github/meetKazuki/Teamwork/badge.svg?branch=develop)](https://coveralls.io/github/meetKazuki/Teamwork?branch=develop)
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { config } from 'dotenv';
+import debug from 'debug';
+import express from 'express';
+
+config();
+
+const app = express();
+const DEBUG = debug('dev');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'welcome to Teamwork' });
+});
+
+app.all('*', (req, res) => {
+  res.status(404).json({ error: 'endpoint doesn\'t exist' });
+});
+
+const port = process.env.NODE_ENV === 'test' ? 6378 : process.env.PORT || 3000;
+app.listen(port, () => DEBUG(`server running on ${port}`));
+
+export default app;
