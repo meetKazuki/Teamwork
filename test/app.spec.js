cf95805866553d0cefad86762670c0fa84bb5241
Desmond Edem <edemintech@gmail.com>
2019-11-12 20:01:05 +0100

    chore(setup): setup project for development
    * setup server-side of the application
    * setup testing framework
    * integrate TravisCI
    * integrate Coveralls
    * integrate CodeClimate

---
+++ test/app.spec.js
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import app from '../src';
+
+describe('ensure app is setup properly', () => {
+  it('app should be a function', () => {
+    expect(app).to.be.a('function');
+  });
+
+  it('should return success on index route', (done) => {
+    request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message).to.eql('welcome to Teamwork');
+        done(err);
+      });
+  });
+
+  it('should return error on an invalid route', (done) => {
+    request(app)
+      .get('/404')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body.error).to.eql('endpoint doesn\'t exist');
+        done(err);
+      });
+  });
+});
